1878452cbd58c14522366d5b66bd77847f982bcc
DinosMaz <infomisoffice@gmail.com>
2022-06-30 20:39:18 +0300

    create todosjs

--- JS Crash Course/main.js
+++ JS Crash Course/main.js
@@ -12,7 +12,11 @@ const person = {
     state: 'Macedonia'
   }
 }
-console.log (person);
-console.log (person.firstName, person.lastName);
+console.log(person);
+console.log(person.firstName, person.lastName);
 console.log(person.hobbies[1]);
 console.log(person.adress.city)
+//αυτά ειναι Object with array and an object inside
+person.email='azazaz11@gmail.com'
+console.log (person);
+// here we added email to the person Object, so now it shows full info
