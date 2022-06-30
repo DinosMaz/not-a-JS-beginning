027b91c4f0ad7a8b4c1935c35c36a062abbe1e87
DinosMaz <infomisoffice@gmail.com>
2022-06-30 20:02:23 +0300

    Update main.js

--- JS Crash Course/main.js
+++ JS Crash Course/main.js
@@ -1,3 +1,18 @@
-let name='John', surname = 'Wick', age=30;
-console.log (`Hello, my name is ${name} ${surname} and I am ${age} years old`);
+let nameex='John', surnameex = 'Wick', ageex=30;
+console.log (`Hello, my name is ${nameex} ${surnameex} and I am ${ageex} years old`);
 //this is another way to put text and variables togeither, thanks to ES6
+const person = {
+  firstName: 'Gianns',
+  lastName: 'thegreat',
+  age: 78,
+  hobbies: ['music', 'movies', 'sports'],
+  adress: {
+    street: 'Monastiriou 50',
+    city: 'Thessaloniki',
+    state: 'Macedonia'
+  }
+}
+console.log (person);
+console.log (person.firstName, person.lastName);
+console.log(person.hobbies[1]);
+console.log(person.adress.city)
