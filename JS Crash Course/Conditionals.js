46ac8f93e48fe2a21238436272081682f655073f
DinosMaz <infomisoffice@gmail.com>
2022-07-01 14:59:38 +0300

    Conditionals updates

--- JS Crash Course/Conditionals.js
+++ JS Crash Course/Conditionals.js
@@ -9,8 +9,8 @@ if (x===10){
 }
 //so, the defference between == and === is that the first checks the content but the second checks the type too
 // lets check another example
-const y='20';
-if (y===20){
+const y=20;
+if (y==10){ //this was changed like the previous line.
   alert('noob') //and this didnt work, but if y==20, than it does work cause type string<>number
 }
 if (y===10){
@@ -26,3 +26,20 @@ if (z===10){
 } else {
   console.log('Z is more than 10')
 };
+
+if (x===10 || y===20) { //so || is for 'OR' and && is for 'AND'. we all know what it means right? 
+  console.log('X is 10 or Y is 20')
+}
+//lets check sth else
+const ak=47; //so if ak=47, than Yes, else NO 9check the syntax of ? and  : and ;
+const guns = ak==47? 'Yes':'No';
+console.log(guns) //dont cl Ak but GUNS
+
+//lets check Switch, it is like options, you ll see, easy logic
+let myname ='JohnWick';
+switch(myname) {
+  case 'Dinos': console.log('My name is Dinos'); break;
+  case 'Methos': console.log('My name is Methos'); break;
+  case 'JackofBlades': alert('I am DEATH, I am JackofBlades'); break;
+  default: console.log('My name is not Dinos or Methos, and you wont die'); break; 
+}
