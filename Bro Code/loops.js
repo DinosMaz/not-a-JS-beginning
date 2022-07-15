355fc962e3718de87d0a3af4b507888a9614f57d
DinosMaz <infomisoffice@gmail.com>
2022-07-15 17:18:07 +0300

    Update loops.js

--- Bro Code/loops.js
+++ Bro Code/loops.js
@@ -1,10 +1,26 @@
 // While loop that repeats some code while the loop's condition remains true
-console.log(`--------------LOOPS-------------------------`)
+console.log(`-----------------LOOPS-------------------------`)
 
 
-var creditNum = window.prompt("YOUR COMPUTER GOT VIYUS! ENTER YOUR CREDIT CARD OR DIE")
+var creditNum = window.prompt("YOUR COMPUTER GOT VIYUS! ENTER YOUR CREDIT CARD OR DIE. . . . .")
 
-while (creditNum == ""){
+// let one of thes two work
+
+//--------------------------------------------------------------------------------------------
+
+/*     while (creditNum == ""){
   creditNum = window.prompt("YOUR COMPUTER GOT VIYUS! ENTER YOUR CREDIT CARD OR DIE")
 }
-console.log ('Thank you your computer no longer has a virus')
+console.log ('Thank you your computer no longer has a virus')     */
+
+//---------------------------------------------------------------------------------------------
+
+/*     do {creditNum = window.prompt("YOUR COMPUTER GOT VIYUS! ENTER YOUR CREDIT CARD OR DIE")
+} while (creditNum=="");
+console.log('Thank you your computer no longer has a virus')     */
+
+console.log(`----------------FOR LOOPS------------------------`)
+// for loop that repeats a limites amount of times for:
+// declare index, 
+// condiotion, 
+// step,
