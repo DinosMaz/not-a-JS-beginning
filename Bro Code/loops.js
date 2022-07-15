3529eb4b8e135e94e1f492d985c7bb4ef17caf02
DinosMaz <infomisoffice@gmail.com>
2022-07-15 17:45:04 +0300

    Update loops.js

--- Bro Code/loops.js
+++ Bro Code/loops.js
@@ -2,25 +2,34 @@
 console.log(`-----------------LOOPS-------------------------`)
 
 
-var creditNum = window.prompt("YOUR COMPUTER GOT VIYUS! ENTER YOUR CREDIT CARD OR DIE. . . . .")
+//var creditNum = window.prompt("YOUR COMPUTER GOT VIYUS! ENTER YOUR CREDIT CARD OR DIE. . . . .")
 
 // let one of thes two work
 
 //--------------------------------------------------------------------------------------------
 
 /*     while (creditNum == ""){
-  creditNum = window.prompt("YOUR COMPUTER GOT VIYUS! ENTER YOUR CREDIT CARD OR DIE")
+  creditNum = window.prompt("YOUR COMPUTER GOT VIRUS! ENTER YOUR...")
 }
 console.log ('Thank you your computer no longer has a virus')     */
 
 //---------------------------------------------------------------------------------------------
 
-/*     do {creditNum = window.prompt("YOUR COMPUTER GOT VIYUS! ENTER YOUR CREDIT CARD OR DIE")
+/*do {creditNum = window.prompt("YOUR COMPUTER GOT VIRUS! ENTER YOUR...")
 } while (creditNum=="");
 console.log('Thank you your computer no longer has a virus')     */
 
 console.log(`----------------FOR LOOPS------------------------`)
 // for loop that repeats a limites amount of times for:
-// declare index, 
-// condiotion, 
-// step,
+// declare index, to track how many times sth happends
+// condiotion, to know where to stop
+// step, what to do next
+
+for (let i=10; i>0; i--){//as long as i>0, do the {} and next do i--
+  console.log(i)
+}
+o=0;
+for (; o<=10;){
+  console.log ("we count to", o)
+  o++
+} console.log (`HAPPY BIRTHDAY`);
